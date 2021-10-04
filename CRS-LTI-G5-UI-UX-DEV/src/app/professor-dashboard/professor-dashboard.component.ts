eefa3f7fc3d028d43e2beae3dd6954cf2577f11d
dontcaredood <65106249+dontcaredood@users.noreply.github.com>
2021-10-04 18:05:17 +0530

    Update professor-dashboard.component.ts

--- CRS-LTI-G5-UI-UX-DEV/src/app/professor-dashboard/professor-dashboard.component.ts
+++ CRS-LTI-G5-UI-UX-DEV/src/app/professor-dashboard/professor-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router'
 
 @Component({
   selector: 'app-professor-dashboard',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfessorDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
+ngOnInit(): void {
 
-  ngOnInit(): void {
+
+  }
+  viewEnrolledStudents(){
+    this.router.navigate(['viewEnrolledStudents'], {relativeTo:this.route});
+  }
+  viewCourses(){
+    this.router.navigate(['viewCourses'], {relativeTo:this.route});
+  }
+
+  addGrade()
+  {
+    this.router.navigate(['addGrade'],{relativeTo:this.route});
   }
 
 }
