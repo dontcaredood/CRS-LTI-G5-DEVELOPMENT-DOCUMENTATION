06155bfdda76794b9027fae7810ec11f977320c7
dontcaredood <65106249+dontcaredood@users.noreply.github.com>
2021-10-04 18:07:50 +0530

    Update view-enrolled-student.component.ts

--- CRS-LTI-G5-UI-UX-DEV/src/app/professor-dashboard/view-enrolled-student/view-enrolled-student.component.ts
+++ CRS-LTI-G5-UI-UX-DEV/src/app/professor-dashboard/view-enrolled-student/view-enrolled-student.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NGXLogger } from 'ngx-logger';
+import { ProfessorService } from 'src/app/service/professor.service';
 
 @Component({
   selector: 'app-view-enrolled-student',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewEnrolledStudentComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private router:Router,private _http:ProfessorService) {   }
+  public   model: any = [];
+   // public models=[];
   ngOnInit(): void {
+  }
+
+  viewEnrolledStudents(){
+
+    var professorId=this.model.professorId;
+
+  this._http.viewEnrolledStudents(professorId)
+  .subscribe(
+  response => {
+    this.model=[];
+    console.log(response);
+    this.router.navigate(['professorDashboardComponent/viewEnrolledStudents'])
+
+    for( let i in response) {   //Pay attention to the 'in'
+    console.log("Reponse in Json: "+response);
+    this.model.push(response[i]);
+    }
+
+  },
+  error => {
+    console.log(error);
+  });
   }
 
 }
