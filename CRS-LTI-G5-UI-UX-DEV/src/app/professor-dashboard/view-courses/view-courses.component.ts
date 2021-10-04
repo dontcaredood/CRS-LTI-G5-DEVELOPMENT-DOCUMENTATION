977014e6962d3eea4b4cdcef0e19ad1fd0410125
dontcaredood <65106249+dontcaredood@users.noreply.github.com>
2021-10-04 18:06:21 +0530

    Update view-courses.component.ts

--- CRS-LTI-G5-UI-UX-DEV/src/app/professor-dashboard/view-courses/view-courses.component.ts
+++ CRS-LTI-G5-UI-UX-DEV/src/app/professor-dashboard/view-courses/view-courses.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NGXLogger } from 'ngx-logger';
+import { ProfessorService } from 'src/app/service/professor.service';
 
 @Component({
   selector: 'app-view-courses',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-courses.component.css']
 })
 export class ViewCoursesComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router:Router,private _http:ProfessorService) {   }
+  public   listOfCourses: any = [];
+  public warningMessage:string='';
 
   ngOnInit(): void {
   }
+
+  viewCourses(){
+
+    var professorId=this.listOfCourses.professorId;
+
+  this._http.viewCourses(professorId)
+  .subscribe(
+  response => {
+    this.listOfCourses=[];
+    console.log(response);
+    this.router.navigate(['professorDashboardComponent/viewCourses'])
+
+    for( let i in response) {  
+    console.log("Reponse in Json: "+response);
+    this.listOfCourses.push(response[i]);
+
+    }
+
+  },
+  error => {
+    console.log(error);
+  });
+  }
+
 
 }
